51715b489fceade71b04b860c1c1881b641e2cc6
IT21257872 <it21257872@my.sliit.lk>
2024-06-08 12:39:16 +0530

    final

--- dbCon.js
+++ dbCon.js
@@ -1,11 +1,31 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const dbConnection = () =>{
-    const MONGO_URL = "mongodb+srv://janith:123@cluster0.v4scjf7.mongodb.net/?retryWrites=true&w=majority"
+// const dbConnection = () =>{
+//     const MONGO_URL = "mongodb+srv://janith:123@cluster0.v4scjf7.mongodb.net/?retryWrites=true&w=majority"
 
-    mongoose.connect(MONGO_URL, ()=> {
-        console.log("Database connection Success")
-    })
-}
+//     mongoose.connect(MONGO_URL, ()=> {
+//         console.log("Database connection Success")
+//     })
+// }
 
-module.exports = {dbConnection} 
+// module.exports = { dbConnection }
+
+const mongoose = require("mongoose");
+
+const dbConnection = () => {
+  try {
+    const MONGO_URL =
+      "mongodb+srv://mohanmalika99:sd6tD4Epd4ZfvTPc@cluster0.xcvaaeu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+    const connectToMongo = async () => {
+      await mongoose.connect(MONGO_URL);
+      console.log("Connected to MongoDB");
+    };
+
+    connectToMongo();
+  } catch (error) {
+    console.log("Database connection failed", error);
+  }
+};
+
+module.exports = { dbConnection };
